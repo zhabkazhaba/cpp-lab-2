6f378c818eae831711795179aac44513be9d4e0d
zhabkazhaba <zhabkazhaba@mail.ru>
2023-09-26 19:52:09 +0300

    without pow() function

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
-#include <cmath>
-using namespace std;
+#include <iomanip>
 
 long int dfactorial (int n){
 	if (n == 0 or n == 1){
@@ -10,19 +9,22 @@ long int dfactorial (int n){
 }
 
 double calculateT(float x){
-	long double numerator = 0;
-	long double denominator = 0;
-	for (int k = 0; k <= 10; k++){
-		numerator += (pow(x, 2*k + 1)) / (dfactorial(2*k+1));
-		denominator += (pow(x,2*k)) / (dfactorial(2*k));
+	double numerator = x;
+	double denominator = 1; // k = 0 values. made this so I don`t have to deal with power of 0
+	double incr = x*x;
+	for (int k = 1; k <= 10; k++){
+		denominator += (incr) / (dfactorial(k<<1));
+		incr = incr*x;
+		numerator += (incr) / (dfactorial((k<<1)+1));
+		incr = incr*x;
 	}
 	return numerator/denominator;
 }
 
 int main(){
 	double y;
-	cout << "Введите y:"; cin >> y;
-	double total = (7 * calculateT(0.25) + 2 * calculateT(1 + y)) / (6 - calculateT(pow(y,2) - 1));
-	cout << total;
+	std::cout << "Введите y:"; std::cin >> y;
+	double total = (7 * calculateT(0.25) + 2 * calculateT(1 + y)) / (6 - calculateT(y*y - 1));
+	std::cout << std::setprecision(15) << total;
 	return 0;
 }
